3a8b01383d34cdd8f34282323cac093ff957eb86
vijay <vijay@gmail.com>
2026-06-11 07:48:19 +0530

    version-2

--- p1.py
+++ p1.py
@@ -3,3 +3,22 @@ if str(a)==str(a)[::-1]:
      print(a,"its palandrom")
 else:
      print(a,"its not palandrom")
+
+
+positive = []
+negative = []
+zero = []
+
+a1 = list(range(-25, 26))
+
+for i in a1:
+    if i > 0:
+        positive.append(i)
+    elif i < 0:
+        negative.append(i)
+    else:
+        zero.append(i)
+
+print("Positive:", positive)
+print("Negative:", negative)
+print("Zero:", zero)
